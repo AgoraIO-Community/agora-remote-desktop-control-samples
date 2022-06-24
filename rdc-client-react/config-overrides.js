22d4017bf2b819546e5b4cac48873098e0e21396
xueqingxiao <xueqingxiao@agora.io>
2022-06-24 16:39:21 +0800

    chore: switch pnpm

--- rdc-client-react/config-overrides.js
+++ rdc-client-react/config-overrides.js
@@ -1,10 +1,10 @@
-const { override, addWebpackExternals, addWebpackPlugin, addWebpackResolve } = require('customize-cra');
+const { override, addWebpackExternals, addWebpackPlugin } = require('customize-cra');
 const webpack = require('webpack');
 
 const target = process.env.TARGET || 'dev';
 
 const API_HOSTS = {
-  dev: 'http://10.103.2.105:3031', // please replace ip with your local network
+  dev: 'https://rdc-api.gz3.agoralab.co', // please replace ip with your local network
   testing: 'https://rdc-api.gz3.agoralab.co',
 };
 
@@ -12,9 +12,6 @@ module.exports = override(
   addWebpackExternals({
     'agora-rdc-core': 'commonjs2 agora-rdc-core',
     'agora-electron-sdk': 'commonjs2 agora-electron-sdk',
-  }),
-  addWebpackResolve({
-    fallback: { querystring: false },
   }),
   addWebpackPlugin(
     new webpack.DefinePlugin({
