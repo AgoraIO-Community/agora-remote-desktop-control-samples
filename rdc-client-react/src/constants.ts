16303523237e2c8a268b7d8bebbbe0fef9eb2fe0
xueqingxiao <xueqingxiao@gmail.com>
2022-07-26 15:05:06 +0800

    fix: update bitrate

--- rdc-client-react/src/constants.ts
+++ rdc-client-react/src/constants.ts
@@ -5,9 +5,9 @@ export const RESOLUTION_BITRATE: {
     bitrate: number;
   };
 } = {
-  '480p600': { width: 848, height: 480, bitrate: 600 },
-  '720p1000': { width: 1280, height: 720, bitrate: 1000 },
-  '1080p2000': { width: 1920, height: 1080, bitrate: 2000 },
+  '480p600': { width: 848, height: 480, bitrate: 1200 },
+  '720p1000': { width: 1280, height: 720, bitrate: 2400 },
+  '1080p2000': { width: 1920, height: 1080, bitrate: 4000 },
 };
 
 export const FRAME_RATES: number[] = [10, 15, 30, 60];
